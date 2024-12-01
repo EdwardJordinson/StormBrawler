44f3bcd5c2c4296e0069ad5549f0ebfdf85e6882
Kim Doleman <kimdoleman@protonmail.com>
2024-12-02 00:32:37 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+/*#include <GLFW/glfw3.h>
+
+
+void error_callback(int, const char* err_str)
+{
+    std::cout << "\nGLFW Error: " << err_str << std::endl;
+}
+
+int main(int arg_count, char *args[])
+{
+
+    glfwSetErrorCallback(error_callback);
+
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    
+    printf("Running against GLFW %i.%i.%i\n", major, minor, revision);
+
+    printf(glfwGetVersionString());
+
+
+    if (!glfwInit())
+    {
+        std::cout << "Has failed" << std::endl;
+    }
+
+    return 0;
+
+}
+*/
